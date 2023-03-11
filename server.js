fb67952b8a05dde1d7efe91c488d7bc9c321f712
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-03-11 12:00:33 -0800

    .gitignore server.js routes

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
-const path = require('path');
+
 
 const app = express();
 const PORT = process.env.port || 3001;
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+
+app.listen(PORT, () =>
+    console.log(`Serving static asset routes at http://localhost:${PORT}`)
+);
