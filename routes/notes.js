6815f9bcebb8d5907a1b97da3efaae8429af07d7
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-03-11 18:20:15 -0800

    uuid, fsutils, notes.js post and get req

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,36 @@
 const notes = require('express').Router();
+const { json } = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { response } = require('.');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const fileName = './db/db.json';
 
 notes.get('/', (req, res) => {
-    res.json("GET route hit");
+    readFromFile(fileName).then((data) => {
+        res.json(JSON.parse(data))
+    });
 })
 
 notes.post('/', (req, res) => {
-    res.json("POST route hit");
+    console.log(req.body);
+
+    const { isValid, errors } = req.body
+    const payload = {
+        title: `${title}`,
+        text: `${text}`,
+        time: Date.now(),
+        error_id: uuidv4(),
+        errors
+    };
+    if (!isValid) {
+        readAndAppend(payload, fileName);
+        res.json('note saved')
+    } else {
+        res.json({
+            message: 'Object is not valid',
+            error_id: payload.error_id
+        })
+    }
 })
 
 module.exports = notes;
